3cb3307b7f02b9c9f66773b271e464f97c8e48b0
ponyo877 <keisuke877jp@yahoo.co.jp>
2025-01-25 14:11:16 +0900

    add upload file validation to POST /upload

--- backend/usecase/usecase.go
+++ backend/usecase/usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -38,18 +39,21 @@ func (u *Usecase) GenerateOGPPage(title, description, name, siteURL string, file
 		"image/png":  "png",
 		"image/jpeg": "jpg",
 	}
-	ext := contentTypeToExtension[contentType]
+	ext, ok := contentTypeToExtension[contentType]
+	if !ok {
+		return "", fmt.Errorf("unsupported content type")
+	}
 	filename := hash + "." + ext
 
 	if err := u.repository.PutFile(filedata, filename, contentType); err != nil {
 		return "", err
 	}
-	imageURL := fileDomain + filename
+	imageURL := fileDomain + "/" + filename
 	site := domain.NewSite(hash, title, description, name, siteURL, imageURL)
 	if err := u.repository.CreateSite(site); err != nil {
 		return "", err
 	}
-	return ogpPageDomain + hash, nil
+	return ogpPageDomain + "/" + hash, nil
 }
 
 func (u *Usecase) GetOGPPage(hash string) (string, error) {
